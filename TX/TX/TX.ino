568f56b628b65c13fd733f7f30804ed86f376b82
kntzn <kntznart@gmail.com>
2019-08-03 19:57:57 +0300

    [Add]: pinout [Add]: LED test

--- TX/TX/TX.ino
+++ TX/TX/TX.ino
@@ -4,6 +4,8 @@
  Author:	kntzn
 */
 
+#include "Pinout.h"
+
 #include <Adafruit_SSD1306.h>
 
 #ifndef SSD1306_128_32 
@@ -21,29 +23,27 @@ void initialize ()
     // setups the pins' modes
     
     // SET pin for the HC-12
-    pinMode (2, OUTPUT);
+    pinMode (HC12_SET, OUTPUT);
 
     // Buttons
-    pinMode (3, INPUT_PULLUP);
-    pinMode (5, INPUT_PULLUP);
-    pinMode (6, INPUT_PULLUP);
+    pinMode (GPI_TOP,    INPUT_PULLUP);
+    pinMode (GPI_MIDDLE, INPUT_PULLUP);
+    pinMode (GPI_BOTTOM, INPUT_PULLUP);
 
     // LEDS
-    // #1
-    pinMode (9,  OUTPUT);
-    pinMode (10, OUTPUT);
-    pinMode (11, OUTPUT);
-    // #2
-    pinMode (12, OUTPUT);
-    pinMode (13, OUTPUT);
-    pinMode (A0, OUTPUT);
+    pinMode (LED_2_B,    OUTPUT);
+    pinMode (LED_2_G,    OUTPUT);
+    pinMode (LED_2_R,    OUTPUT);
+    pinMode (LED_1_G,    OUTPUT);
+    pinMode (LED_1_B,    OUTPUT);
+    pinMode (LED_1_R,    OUTPUT);
 
     // Thumb pot
-    pinMode (A1, INPUT);  // Pot in
-    pinMode (A2, OUTPUT); // Pot power
+    pinMode (POT_IN,     INPUT);
+    pinMode (POT_PWR,    OUTPUT);
 
     // VCC in
-    pinMode (A3, INPUT);
+    pinMode (A3,         INPUT);
     }
 
 int main ()
@@ -52,8 +52,63 @@ int main ()
 
     Adafruit_SSD1306 display;
     display.begin (SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
-    display.drawLine (0, 0, 16, 16, WHITE);
-    
+//    display.drawLine (0, 0, 16, 16, WHITE);
+    display.clearDisplay ();
+    display.display ();
+
+    const auto d = 5;
+
+    while (true)
+        { 
+        for (int i = 0; i < 64; i++)
+            { 
+            analogWrite (LED_2_R, 63);
+            analogWrite (LED_2_G, i);
+            analogWrite (LED_2_B, 0);
+            delay (d);
+            }
+
+        for (int i = 0; i < 64; i++)
+            {
+            analogWrite (LED_2_R, 63 - i);
+            analogWrite (LED_2_G, 63);
+            analogWrite (LED_2_B, 0);
+            delay (d);
+            }
+
+        for (int i = 0; i < 64; i++)
+            {
+            analogWrite (LED_2_R, 0);
+            analogWrite (LED_2_G, 63);
+            analogWrite (LED_2_B, i);
+            delay (d);
+            }
+
+        for (int i = 0; i < 64; i++)
+            {
+            analogWrite (LED_2_R, 0);
+            analogWrite (LED_2_G, 63 - i);
+            analogWrite (LED_2_B, 63);
+            delay (d);
+            }
+
+        for (int i = 0; i < 64; i++)
+            {
+            analogWrite (LED_2_R, i);
+            analogWrite (LED_2_G, 0);
+            analogWrite (LED_2_B, 63);
+            delay (d);
+            }
+
+        for (int i = 0; i < 64; i++)
+            {
+            analogWrite (LED_2_R, 63);
+            analogWrite (LED_2_G, 0);
+            analogWrite (LED_2_B, 63 - i);
+            delay (d);
+            }
+        }
+
 
     display.display ();
     }
